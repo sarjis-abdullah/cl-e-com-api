ef96a31a5ff4f7f5871be97e3750ced80f4e70e0
psarjis@gmail.com <sarjis.m.a@gmail.com>
2023-09-28 01:57:59 +0600

    more field added

--- app/routes/stripeRoutes.js
+++ app/routes/stripeRoutes.js
@@ -3,13 +3,81 @@ const router = express.Router();
 const dotenv = require("dotenv");
 dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const shippingData = shipping_address_collection = {
+  allowed_countries: ['US', 'CA']
+},
+shipping_options = [
+  {
+    shipping_rate_data: {
+      type: 'fixed_amount',
+      fixed_amount: {
+        amount: 0,
+        currency: 'usd',
+      },
+      display_name: 'Free shipping',
+      delivery_estimate: {
+        minimum: {
+          unit: 'business_day',
+          value: 5,
+        },
+        maximum: {
+          unit: 'business_day',
+          value: 7,
+        },
+      },
+    },
+  },
+  {
+    shipping_rate_data: {
+      type: 'fixed_amount',
+      fixed_amount: {
+        amount: 1500,
+        currency: 'usd',
+      },
+      display_name: 'Next day air',
+      delivery_estimate: {
+        minimum: {
+          unit: 'business_day',
+          value: 1,
+        },
+        maximum: {
+          unit: 'business_day',
+          value: 1,
+        },
+      },
+    },
+  },
+]
+const invoice_creation = {
+  enabled: true,
+  invoice_data: {
+    description: 'Invoice for Product X',
+    metadata: {
+      order: 'order-xyz',
+    },
+    account_tax_ids: [123456789],
+    custom_fields: [
+      {
+        name: 'Purchase Order',
+        value: 'PO-XYZ',
+      },
+    ],
+    rendering_options: {
+      amount_tax_display: 'include_inclusive_tax',
+    },
+    footer: 'B2B Inc.',
+  },
+}
+const CURRENCY = "usd"
 
 router.get("/create-payment-intent", async (req, res) => {
   const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    shipping_options,
     line_items: [
       {
         price_data: {
-          currency: 'usd',
+          currency: CURRENCY,
           product_data: {
             name: 'T-shirt',
           },
@@ -25,7 +93,7 @@ router.get("/create-payment-intent", async (req, res) => {
       },
       {
         price_data: {
-          currency: 'usd',
+          currency: CURRENCY,
           product_data: {
             name: 'Air conditioner',
           },
@@ -43,6 +111,9 @@ router.get("/create-payment-intent", async (req, res) => {
     // automatic_tax: {
     //   enabled: true,
     // },
+    phone_number_collection: {
+      enabled: true,
+    },
     mode: 'payment',
     success_url: 'https://example.com/success',
     cancel_url: 'https://example.com/cancel',
