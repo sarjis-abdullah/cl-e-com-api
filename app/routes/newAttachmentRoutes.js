c4a1e9b3d0b9712042aa20cc02ee2903822cc87c
psarjis@gmail.com <sarjis.m.a@gmail.com>
2023-11-01 00:45:08 +0600

    more error validation added

--- app/routes/newAttachmentRoutes.js
+++ app/routes/newAttachmentRoutes.js
@@ -1,45 +1,54 @@
-const express = require('express');
-const controller = require('../controllers/brandController');
-const { validateCreateItem, validateUpdateItem } = require('../middlewares/brandMiddleware');
+const express = require("express");
+const controller = require("../controllers/brandController");
+const {
+  validateCreateItem,
+  validateUpdateItem,
+} = require("../middlewares/brandMiddleware");
 const multer = require("multer");
-const { setUserData } = require('../middlewares/userMiddleware');
+const { setUserData } = require("../middlewares/userMiddleware");
 const controller2 = require("../controllers/attachmentController");
 const { validateCreateItem2 } = require("../middlewares/attachmentMiddleware");
 const upload = require("../middlewares/multerMiddleware");
 const router = express.Router();
-const Joi = require('joi');
+const Joi = require("joi");
 const createSchema = Joi.object({
   type: Joi.string().required(),
   fileSource: Joi.required(),
   fileName: Joi.required(),
 });
-router.get('/', controller2.getAll);
+router.get("/", controller2.getAll);
 
-router.post("/", setUserData, async (req, res, next) => {
-  try {
-    console.log(76767)
-    await upload.single("fileSource")(req, res, function (err) {
-      if (err instanceof multer.MulterError) {
-        res.status(400).json({ error: "Multer Error", err });
-      } else if (err) {
-        res.status(500).json({ error: "Server Error", err });
-      } else {
-        next()
-      }
-    });
-  } catch (err) {
-    res.status(500).json({ error: "Server Error", err });
-  }
-}, (req, res, next) => {
+router.post(
+  "/",
+  async (req, res, next) => {
+    try {
+      await upload.single("fileSource")(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+          res.status(400).json({ error: "Multer Error", err });
+        } else if (err) {
+          res.status(500).json({ error: "Server Error", err });
+        } else if (req.file && req.file.mimetype) {
+          next();
+        } else {
+          res.status(500).json({ error: "Please give a binary file!", err });
+        }
+      });
+    } catch (err) {
+      res.status(500).json({ error: "Server Error", err });
+    }
+  },
+  (req, res, next) => {
+    const { error } = createSchema.validate(req.body);
 
-  const { error } = createSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({ error: error.details[0].message });
+    }
 
-  if (error) {
-    return res.status(400).json({ error: error.details[0].message });
-  }
-
-  next()
-}, controller2.create);
+    next();
+  },
+  setUserData,
+  controller2.create
+);
 // router.post('/', validateCreateItem, setUserData, controller.create);
 
 // router.get('/:id', controller.getById);
